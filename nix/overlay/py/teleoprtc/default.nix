1f97c963612202c772859b52345a748a573685cf
King Art <arthur@ischium.net>
2026-02-10 02:25:44 +0000

    teleoprtc

---
+++ nix/overlay/py/teleoprtc/default.nix
@@ -0,0 +1,29 @@
+{
+  # python.pkgs
+  buildPyproject,
+  # self
+  teleoprtc-src,
+  # resolved by buildPyproject if not supplied
+  pythonInterpreter ? null,
+}:
+buildPyproject {
+
+  src = teleoprtc-src;
+  patches = [ ./teleoprtc.patch ];
+
+  inherit pythonInterpreter;
+
+  # exclude tests that don't work in the sandbox
+  pytestFlags =
+    map
+      (
+        test: "--deselect=tests/test_integration.py::TestStreamIntegration::${test}"
+      )
+      [
+        "test_multi_camera_1_camera_and_audio"
+        "test_multi_camera_3_camera_and_audio_and_messaging"
+        "test_multi_camera_0_multi_camera"
+        "test_multi_camera_2_camera_and__messaging"
+      ];
+
+}
